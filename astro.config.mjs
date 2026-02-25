c710e19c2a45a9db3c78ebab7e2e27246264b6e7
Alfredo <vargas.alfredojr@gmail.com>
2026-02-24 23:21:24 -0500

    enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  server: {
+    host: "0.0.0.0",
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
+});
